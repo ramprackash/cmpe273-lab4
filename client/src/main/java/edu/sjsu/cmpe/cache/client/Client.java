40bb9b7bd0a605c738677b66d8d2ccab40b6bc34
Ramprakash Ram Mohan <ramprackash@gmail.com>
2015-05-20 14:28:19 -0700

    Changed the PUT to async mode.
    TODO: Support DELETE for rollback

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -4,16 +4,14 @@ public class Client {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+        CacheServiceInterface cache = new DistributedCacheService( "http://localhost", new Integer(3000), new Integer(3));
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
+        System.out.println("put(2 => bar)");
+        cache.put(2, "bar");
 
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
-
-        System.out.println("Existing Cache Client...");
+        String value = cache.get(2);
+        System.out.println("get(2) => " + value);
+        System.out.println("Exiting Cache Client...");
     }
 
 }
--- client/src/main/java/edu/sjsu/cmpe/cache/client/DistributedCacheService.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/DistributedCacheService.java
@@ -3,7 +3,10 @@ package edu.sjsu.cmpe.cache.client;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.async.Callback;
 import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.util.concurrent.Future;
 
 /**
  * Distributed cache service
@@ -11,9 +14,22 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  */
 public class DistributedCacheService implements CacheServiceInterface {
     private final String cacheServerUrl;
+    private final String getUrl;
+    private final Integer startport;
+    private final Integer numservice;
+    private final int writeQuorum;
+    private int numSuccess;
+    private int numWriteAttempts;
 
-    public DistributedCacheService(String serverUrl) {
+    public DistributedCacheService(String serverUrl, Integer startport, Integer numservice) {
+        this.writeQuorum = 2;
+        this.numSuccess = 0;
+        this.numWriteAttempts = 0;
+        this.numservice = numservice;
         this.cacheServerUrl = serverUrl;
+        this.startport = startport;
+        this.getUrl = this.cacheServerUrl + ":" + this.startport.toString();
+
     }
 
     /**
@@ -23,7 +39,7 @@ public class DistributedCacheService implements CacheServiceInterface {
     public String get(long key) {
         HttpResponse<JsonNode> response = null;
         try {
-            response = Unirest.get(this.cacheServerUrl + "/cache/{key}")
+            response = Unirest.get(this.getUrl + "/cache/{key}")
                     .header("accept", "application/json")
                     .routeParam("key", Long.toString(key)).asJson();
         } catch (UnirestException e) {
@@ -39,20 +55,59 @@ public class DistributedCacheService implements CacheServiceInterface {
      *      java.lang.String)
      */
     @Override
-    public void put(long key, String value) {
+    public void put(final long key, final String value) {
         HttpResponse<JsonNode> response = null;
-        try {
-            response = Unirest
-                    .put(this.cacheServerUrl + "/cache/{key}/{value}")
+        for (int i = 0 ; i < this.numservice.intValue(); i++) {
+            Integer port = this.startport + new Integer(i);
+
+            String cacheUrl = this.cacheServerUrl + ":" + port.toString();
+            System.out.println("Putting to " + cacheUrl);
+
+            Future<HttpResponse<JsonNode>> future = Unirest
+                    .put(cacheUrl + "/cache/{key}/{value}")
                     .header("accept", "application/json")
                     .routeParam("key", Long.toString(key))
-                    .routeParam("value", value).asJson();
-        } catch (UnirestException e) {
-            System.err.println(e);
-        }
+                    .routeParam("value", value).asJsonAsync(new Callback<JsonNode>() {
+                        private void checkForSuccess() {
+                            System.out.println(DistributedCacheService.this.numWriteAttempts + " of " + DistributedCacheService.this.numservice);
+                            if (DistributedCacheService.this.numWriteAttempts == DistributedCacheService.this.numservice) {
+                                if (DistributedCacheService.this.numSuccess >= DistributedCacheService.this.writeQuorum) {
+                                    System.out.println("PUT COMPLETED SUCCESSFULLY");
+                                } else {
+                                    System.out.println("PUT FAILED - NEED TO ROLLBACK");
+                                }
+                                DistributedCacheService.this.numWriteAttempts = DistributedCacheService.this.numSuccess = 0;
+                            }
+                        }
 
-        if (response.getCode() != 200) {
-            System.out.println("Failed to add to the cache.");
+                        @Override
+                        public void completed(HttpResponse<JsonNode> httpResponse) {
+                            DistributedCacheService.this.numWriteAttempts++;
+                            if (httpResponse.getCode() != 200) {
+                                System.out.println("Failed to add to the cache.");
+                            }
+                            else {
+                                System.out.println("Added " + key + " => " + value);
+                                DistributedCacheService.this.numSuccess++;
+                            }
+                            checkForSuccess();
+                        }
+
+                        @Override
+                        public void failed(UnirestException e) {
+                            DistributedCacheService.this.numWriteAttempts++;
+                            System.out.println("Failed : " + e);
+                            checkForSuccess();
+                        }
+
+                        @Override
+                        public void cancelled() {
+                            DistributedCacheService.this.numWriteAttempts++;
+                            System.out.println("Cancelled");
+                            checkForSuccess();
+                        }
+                    });
+
         }
     }
 }
